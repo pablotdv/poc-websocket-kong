966a530b3ec974cd514277c3717d56a73db0672a
Pablo T. de Vargas <pablotdvsm@gmail.com>
2023-08-31 13:48:46 -0300

    poc websocket

--- src/Common/Models/MongoContext.cs
+++ src/Common/Models/MongoContext.cs
@@ -5,13 +5,12 @@ namespace Common.Models;
 
 public class MongoContext
 {
-    private readonly IMongoDatabase _database = null;
+    private readonly IMongoDatabase _database;
 
     public MongoContext(IConfiguration configuration)
     {
         var client = new MongoClient(configuration.GetConnectionString("DefaultConnection"));
-        if (client != null)
-            _database = client.GetDatabase("Chat");
+        _database = client.GetDatabase("Chat");
     }
 
     public IMongoCollection<Contato> Contatos
